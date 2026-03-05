7d439c5a6afe6cade18b441ff9e5477b2d76356e
Sakshidubey13 <sakshidubey1304@gmail.com>
2026-03-05 14:52:36 +0530

    ..

---
+++ portfolio/src/Components/About/About.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="container about">
+      <h2>About Me</h2>
+
+      <p>
+        Hello! My name is Sakshi Dubey. I am learning Web Development
+        and currently working with HTML, CSS, JavaScript and React JS.
+      </p>
+
+      <p>
+        I love building simple and beautiful websites and improving my
+        programming skills every day.
+      </p>
+    </div>
+  );
+}
+
+export default About;
