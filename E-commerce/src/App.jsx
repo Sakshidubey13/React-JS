8b38f98db71293ede27b10cb59d234e142828ecd
Sakshidubey13 <sakshidubey1304@gmail.com>
2026-03-28 12:32:11 +0530

    ..

--- E-commerce/src/App.jsx
+++ E-commerce/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import {Routes, Route} from 'react-router'
+import Home from './components/Home/Home'
+import Navbar from './components/Navbar/Navbar'
 
 export default function App() {
   return (
     <div>
       <Routes>
-        
+          <Route path='/' element={<Home/>} /> 
+          <Route path='/navbar' element={<Navbar/>}/> 
       </Routes>
     </div>
   )
