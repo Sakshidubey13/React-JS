c07692d0147eefd2b01caa1585785570acca510d
Sakshidubey13 <sakshidubey1304@gmail.com>
2026-04-08 17:59:00 +0530

    ..

--- ecom-app/src/components/home/Home.jsx
+++ ecom-app/src/components/home/Home.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
 
 export default function home() {
+  useEffect(()=>{
+  fetchProducts();
+  },[]);
+
+//async and .then 
+    async function fetchProducts(){
+     const rec = await fetch("https://dummyjson.com/products");
+        const data = await res.json();
+        console.log(data);
+    }  
+
   return (
     <div>
         <h1>hii</h1>
