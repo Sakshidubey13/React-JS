5f91d4303924d54a644868753c8827144bd99b56
Sakshidubey13 <sakshidubey1304@gmail.com>
2026-04-09 14:57:16 +0530

    ..

--- twitter/components/SignUp.jsx
+++ twitter/components/SignUp.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
+
 
 export default function SignUp() {
+
+    const [user,setUser] = useState({})
+
+
+    //api calling and network request
+    //.post => to send data 
+    const handleSignup = async() => {
+      axios.post("")
+    };
+
+
   return (
     <div>
-      <input type="text" />
+
+      <div>
+        <input onChange={(e)=>setUser({...user,name:e.target.value})} type="text" placeholder="Full Name" />
+      </div>
+
+      <div>      
+        <input onChange={(e)=>setUser({...user,email:e.target.value})} type="text" placeholder="Email" />
+      </div>
+
+      <div>     
+        <input onChange={(e)=>setUser({...user,phone:e.target.value})} type="text" placeholder="Phone" />
+      </div>
+
+      <div>       
+        <input onChange={(e)=>setUser({...user,password:e.target.value})} type="text" placeholder="Password" />
+      </div>
+
+      <div>
+        <button onClick={handleSignup}>Sign Up</button>
+      </div>
+
     </div>
   );
 }
