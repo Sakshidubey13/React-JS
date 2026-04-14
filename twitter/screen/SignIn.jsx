6313129a98129bb4302eaa81024183ee225b2256
Sakshidubey13 <sakshidubey1304@gmail.com>
2026-04-14 17:06:07 +0530

    ..

--- twitter/screen/SignIn.jsx
+++ twitter/screen/SignIn.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { users_api } from "../utils/api.js";
 
 export default function SignIn() {
+  const [user, setUser] = useState({});
+
+  const handleSignIn = async () => {
+    const res = await axios.get(users_api);
+    const loginUser = res.data.find((e) => e.email == user.email && e.password == user.password )
+    localStorage.setItem("current-user",JSON.stringify(loginUSer));
+  };
   return (
     <div>
-
-        
-      
+      <input
+        type="text"
+        placeholder="Email"
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+      />
+      <input
+        type="text"
+        placeholder="password"
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+      />
+      <button onClick={handleSignIn}>SignIn</button>
     </div>
-  )
+  );
 }
-
--- twitter/src/App.jsx
+++ twitter/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import SignUp from '../screen/SignUp'
 import {Routes,Route} from 'react-router'
+import HomePage from '../screen/HomePage'
+import SignIn from '../screen/SignIn'
+
 
 export default function App() {
   return (
 
     <div>
-      <SignUp/>
+      <Routes>
+        <Route path='/' element={<SignUp/>}/>
+        <Route path='/home' element={<HomePage/>}/>
+        <Route path='/signin' element={<SignIn/>}/>
+      </Routes>
    
     </div>
   )
