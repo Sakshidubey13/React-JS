a795b29a3beedd2795b510b6eda114dbf6009a55
Dubey Sakshi <sakshidubey1304@gmail.com>
2026-05-27 15:28:12 +0530

    ..

--- User-App/src/Components/user.jsx
+++ User-App/src/Components/user.jsx
@@ -1,21 +1,51 @@
 //import React from 'react'
 import allUser from "../data/user.js";
+import { useState } from "react";
 
+const User = () => {
+  const [users, setUsers] = useState(allUser);
+  return (
+    <div>
+      <h1>All Users</h1>
+      {users.map((user, i) => (
+        <div key={i}>
+          <hr />
+          <h3>{user.name}</h3>
+          <p>{user.email}</p>
+          <button
+            onClick={() => {
+              const temp = [...users];
+              temp.splice(i, 1);
+              setUsers(temp);
+            }}
+          >
+            Remove
+          </button>
+        </div>
+      ))}
+      {/*{allUser.length} find length any json data */}
+      <button
+        onClick={() => {
+          const temp = [...users];
+          temp.push({ name: "Sakshi", email: "sakshiid13@gmail.com" });
+          setUsers(temp);
+          //setUsers([...users,{name:"Sakshi",email:"sakshiid13@gmail.com"}])
+        }}
+      >
+        Add
+      </button>
 
-const User =() =>{
-    return <div>
-        <h1>All USers</h1>
-       {
-    
-        allUser.map((user,i)=><div key={i}>
-            <hr/>
-            <h3>{user.name}</h3>
-            <p>{user.email}</p>
-            {/*<hr/>*/}
-        </div>)
-       }
-       {/*{allUser.length}*/}
-    </div>;
-}
+      <button
+        onClick={() => {
+          const temp = [...users];
+          temp.pop();
+          setUsers(temp);
+        }}
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
 
 export default User;
